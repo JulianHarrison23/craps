0f06dd83c9ca8769ac44c6ff600ba7c779ce6310
Julian Harrison <jharrison23@students.polytechnic.org>
2022-01-20 14:49:10 -0800

    Fixed issue with pressing Enter to roll

--- Craps.java
+++ Craps.java
@@ -20,16 +20,16 @@ public class Craps {
             else{
                 System.out.println("The instructions are simple. On your first roll, you roll two die. If the sum is equal to 2, 3, or 12 then you lose. However, if your roll is 7 or 11 you win. If your sum is none of the above, then your sum becomes your 'point'. Your goal on your next rolls is to roll the two die to be equalivent to the first sum before rolling a 7. If a 7, is rolled first then you lose. On the contrary, if the sum is rolled first then you win!");
                 System.out.println();
-                System.out.println("To play, please press 'x' before enter each turn to roll the dice! Good luck!");
+                System.out.println("To play, please press <Enter> each turn to roll the dice! Good luck!");
             }
         }
         else{
             apple = false;
         }
         while(apple){
-            System.out.println("Press <x + Enter> to roll...");
+            System.out.println("Press <Enter> to roll...");
             Scanner pause = new Scanner(System.in);
-            String pause1 = input.next();
+            String pause1 = pause.nextLine();
             Die Die1 = new Die();
             Die Die2 = new Die();
             int roll11 = Die1.getRoll();
@@ -43,8 +43,8 @@ public class Craps {
             }
             else{
                 System.out.println("You rolled a "+ roll11+ " and a "+ roll12 + " on your first roll. Your point is "+sum1+".");
-                System.out.println("Press <x + Enter> to roll...");
-                pause1 = input.next();
+                System.out.println("Press <Enter> to roll...");
+                pause1 = pause.nextLine();
                 Die1.newRoll();
                 Die2.newRoll();
                 int roll21 = Die1.getRoll();
@@ -52,8 +52,8 @@ public class Craps {
                 int sum2 = roll21 + roll22;
                 while(sum2!=sum1&&sum2!=7){
                     System.out.println("You rolled a "+ roll21+ " and a "+ roll22 + " on your roll. This equals "+ sum2+". Your point is still "+sum1+".");
-                    System.out.println("Press <x + Enter> to roll...");
-                    pause1 = input.next();
+                    System.out.println("Press <Enter> to roll...");
+                    pause1 = pause.nextLine();
                     Die1.newRoll();
                     Die2.newRoll();
                     roll21 = Die1.getRoll();
