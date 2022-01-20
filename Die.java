088cdb5923b30c87d9101fefee170892555257e9
Julian Harrison <jharrison23@students.polytechnic.org>
2022-01-19 22:15:46 -0800

    Created Die.java class

---
+++ Die.java
@@ -0,0 +1,19 @@
+/**
+ * File for a Song class to be used in the Playlist Project
+ * @author
+ * @version
+ */
+public class Die {
+    //Fields-- what information do we want each Song to store?
+    private int roll;
+
+    public Die(){
+        roll = (int)(Math.random()*6)+1;
+    }
+
+    public int getRoll(){
+        return roll;
+    }
+
+
+}
